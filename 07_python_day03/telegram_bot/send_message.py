30315f4cca7c7f21dfcf2b49a0a226f3c8e3eef7
moonhanna <mhn97@naver.com>
2019-12-13 11:50:08 +0900

    update_sendmessage.py file

--- 07_python_day03/telegram_bot/send_message.py
+++ 07_python_day03/telegram_bot/send_message.py
@@ -2,8 +2,6 @@ import requests
 from decouple import config
 
 base = 'https://api.telegram.org'
-# token = '827961019:AAEXLmziybDKWqo8bY4jqmSpLFZ2VZBvkjc'
-# chat_id = '1030133925'
 token = config('TELEGRAM_BOT_TOKEN')
 chat_id = config('CHAT_ID')
 text = 'hello'
