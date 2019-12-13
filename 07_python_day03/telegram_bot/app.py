2b3bb8f4de54bd17bd0a43d45832181b7ea4cb27
moonhanna <mhn97@naver.com>
2019-12-13 15:08:37 +0900

    update telegram_bot

--- 07_python_day03/telegram_bot/app.py
+++ 07_python_day03/telegram_bot/app.py
@@ -1,6 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from decouple import config
+import requests
 app = Flask(__name__)
+
+base = 'https://api.telegram.org'
+token = config('TELEGRAM_BOT_TOKEN')
+chat_id = config('CHAT_ID')
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/write')
+def write():
+    return render_template('write.html')
+
+@app.route('/send')
+def send():
+    text = request.args.get('message')
+    url = f'{base}/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
+    requests.get(url)
+    return render_template('send.html')
+
+@app.route(f'/{token}',methods=['POST'])
+def telegram():
+    # step1. 구조 print 해보기 & 변수저장
+    print(request.get_json())
+    from_telegram = request.get_json()
+
+    # step2. 그대로 돌려보내기
+    # NoneType일 경우만 예외처리
+    if from_telegram.get('message') is not None: 
+        chat_id = from_telegram.get('message').get('from').get('id')
+        text = from_telegram.get('message').get('text')
+        url = f'{base}/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
+        requests.get(url)
+        
+    return '', 200
