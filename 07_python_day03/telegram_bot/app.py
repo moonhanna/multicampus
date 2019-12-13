52888e2a1852f8477a1d41ac67a82af79e0c481a
moonhanna <mhn97@naver.com>
2019-12-13 16:17:45 +0900

    update app.py file

--- 07_python_day03/telegram_bot/app.py
+++ 07_python_day03/telegram_bot/app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 base = 'https://api.telegram.org'
 token = config('TELEGRAM_BOT_TOKEN')
 chat_id = config('CHAT_ID')
+naver_client_id = config('NAVER_CLIENT_ID')
+naver_client_secret = config('NAVER_CLIENT_SECRET')
 
 @app.route('/')
 def hello_world():
@@ -33,7 +35,12 @@ def telegram():
     if from_telegram.get('message') is not None: 
         chat_id = from_telegram.get('message').get('from').get('id')
         text = from_telegram.get('message').get('text')
-        url = f'{base}/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
-        requests.get(url)
-        
+
+        if text[0:4] == '/번역 ':
+            headers = {'X-Naver-Client-Id':naver_client_id, 'X-Naver-Client-Secret':naver_client_secret}
+            data = {'source':'ko', 'target':'en', 'text':text[4:]}
+            papago_res = requests.post('https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=data)
+            text = papago_res.json().get('message').get('result').get('translatedText')
+        requests.get(f'{base}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
+
     return '', 200
